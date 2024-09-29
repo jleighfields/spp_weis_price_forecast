0876c178e408c45f2b7b1c5a7e20875c1705cae2
Justin Fields <jleighfields@gmail.com>
2024-09-28 22:36:48 -0600

    added modeling and data eng functions

--- src/data_engineering.py
+++ src/data_engineering.py
@@ -1,296 +1,213 @@
 import os
-import sys
+import shutil
+import pickle
 import random
+import sys
 import numpy as np
 import pandas as pd
-from scipy import stats
-from tqdm import tqdm_notebook as tqdm
+import duckdb
+from typing import List
 
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
+import requests
+from io import StringIO
 
-from darts import TimeSeries, concatenate
-from darts.dataprocessing.transformers import Scaler
-from darts.models import TFTModel
-from darts.metrics import mape, smape, mae, ope, rmse
-from darts.utils.statistics import check_seasonality, plot_acf
-from darts.datasets import AirPassengersDataset, IceCreamHeaterDataset
-from darts.utils.timeseries_generation import datetime_attribute_timeseries
-from darts.utils.likelihood_models import QuantileRegression, GumbelLikelihood, GaussianLikelihood
-
+import ibis
+import ibis.selectors as s
+from ibis import _
+from darts.dataprocessing.transformers import MissingValuesFiller
 from darts import TimeSeries
-from darts.utils.timeseries_generation import (
-    gaussian_timeseries,
-    linear_timeseries,
-    sine_timeseries,
-)
-from darts.models import (
-    TFTModel,
-    LinearRegressionModel,
-    LightGBMModel,
-    RNNModel,
-    TCNModel,
-    TransformerModel,
-    NBEATSModel,
-    BlockRNNModel,
-    VARIMA,
-)
-
-
-from torchmetrics import MeanAbsolutePercentageError
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 import warnings
 warnings.filterwarnings("ignore")
 
+# logging
 import logging
 
 # define log
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-
-########################################
-# Feature engineering
-########################################
-
-########################################
-# fill missing values
-########################################
-def fill_missed_values(df):
-    """
-    """
-    df = df.fillna(method='ffill')
-    df = df.fillna(method='bfill')
-
-    return df
+from src import params
 
 
-########################################
-# lmp
-########################################
-def get_psco_price_df(price_df):
-    """
-    """
-    psco_idx = price_df["PNODE_Name"].str.contains("PSCO", case=False)
-    psco_price_df_long = price_df[psco_idx]
-    psco_price_df = psco_price_df_long.pivot_table(
-                            index='GMTIntervalEnd',
-                            columns='PNODE_Name',
-                            values='LMP_HOURLY',
-                            margins=False,
-                        ).reset_index()
-
-    psco_price_df.columns.name=None
-
-    # get list of node names
-    ls_nodes_name = list(psco_price_df.columns[1:])
-    # separate load area names
-    la_names = [n for n in ls_nodes_name if '_LA' in n]
-    not_la_names = [n for n in ls_nodes_name if '_LA' not in n]
-    random.Random(42).shuffle(not_la_names)
-    # put load area in the front, make sure we train on these
-    ls_nodes_name = la_names + not_la_names
-
-    # return psco_price_df_long to create an overall scaler for psco lmps
-    return fill_missed_values(psco_price_df), ls_nodes_name, psco_price_df_long
+#############################################
+# parameters for column names
+#############################################
+FUTR_COLS = ['MTLF', 'Wind_Forecast_MW', 'Solar_Forecast_MW', 're_ratio', 're_diff']
+PAST_COLS = ['Averaged_Actual']
+Y = ['LMP']
+IDS = ['unique_id']
 
 
-def create_psco_price_series(psco_price_df, node_name_ls):
-    """
-    """
-    psco_price_series = TimeSeries.from_dataframe(
-            psco_price_df, 
-            time_col='GMTIntervalEnd', 
-            value_cols=node_name_ls, 
-            fill_missing_dates=True, 
-            freq='H', 
-            fillna_value=0, 
-            static_covariates=None, 
-            hierarchy=None
-        ).astype(np.float32)
-
-    return psco_price_series
-
-
-########################################
-# mtlf
-########################################
-def remove_duplicates(df):
-    """
-    Removes duplicates in mtlf and mtrf dataframes.
-    """
-    dups = df.GMTIntervalEnd.duplicated()
-    df = df[~dups]
-    return df
-
-
-def create_mtlf_series(mtlf_df):
-    """
-    """
-    # remove duplicates
-    mtlf_df = remove_duplicates(mtlf_df)
-    # fill missing values
-    mtlf_df = fill_missed_values(mtlf_df)
-    # create series
-    mtlf_series = TimeSeries.from_dataframe(mtlf_df, time_col='GMTIntervalEnd', value_cols='MTLF', fill_missing_dates=True, freq='H', fillna_value=0, static_covariates=None, hierarchy=None).astype(np.float32)
-    avg_act_series = TimeSeries.from_dataframe(mtlf_df, time_col='GMTIntervalEnd', value_cols='Averaged_Actual', fill_missing_dates=True, freq='H', fillna_value=0, static_covariates=None, hierarchy=None).astype(np.float32)
+#############################################
+# data prep
+#############################################
+def prep_lmp(loc_filter: str='PSCO_'):
+    con = ibis.duckdb.connect("data/spp.ddb")
+    lmp = con.table('lmp')
+    lmp = lmp.filter(_.Settlement_Location_Name.contains(loc_filter))
+    drop_cols = [
+        'Interval_HE', 'GMTIntervalEnd_HE', 'timestamp_mst_HE',
+        'Settlement_Location_Name', 'PNODE_Name', 
+        'MLC', 'MCC', 'MEC'
+    ]
     
-    return mtlf_series, avg_act_series
+    lmp = (
+        lmp
+        .mutate(unique_id = _.Settlement_Location_Name )
+        .mutate(timestamp_mst = _.timestamp_mst_HE)
+        # .mutate(y = _.LMP) 
+        .drop(drop_cols) 
+        .order_by(['unique_id', 'timestamp_mst'])
+    )
+
+    return lmp
 
 
-def create_mtlf_lmp_series(mtlf_df, psco_lmp_df, list_nodes_name):
-    """
-    """
-    lmp_cols = ['GMTIntervalEnd'] + list_nodes_name
-    mtlf_df = mtlf_df.merge(psco_lmp_df[lmp_cols], on='GMTIntervalEnd', how='left')
-    print(f'mtlf_df.columns: {mtlf_df.columns}')
-    mtlf_df = fill_missed_values(mtlf_df)
-    mtlf_series = TimeSeries.from_dataframe(mtlf_df, time_col='GMTIntervalEnd', value_cols='MTLF', fill_missing_dates=True, freq='H', fillna_value=0, static_covariates=None, hierarchy=None).astype(np.float32)
-    avg_act_cols = ['Averaged_Actual'] + list_nodes_name
-    avg_act_series = TimeSeries.from_dataframe(mtlf_df, time_col='GMTIntervalEnd', value_cols=avg_act_cols, fill_missing_dates=True, freq='H', fillna_value=0, static_covariates=None, hierarchy=None).astype(np.float32)
+def prep_mtrf():
+    con = ibis.duckdb.connect("data/spp.ddb")
+    mtrf = con.table('mtrf')
+    drop_cols = ['Interval', 'GMTIntervalEnd']
+
+    mtrf = (
+        mtrf
+        # .mutate(ds = _.timestamp_mst)
+        .drop(drop_cols) 
+        .order_by(['timestamp_mst'])
+    )
     
-    return mtlf_series, avg_act_series
-          
+    return mtrf
 
-########################################
-# mtrf
-########################################  
-# Add renewable/load ratio feature to mtrf dataframe
-def add_enrgy_ratio_to_mtrf(mtlf_df, mtrf_df):
-    """
-    """
-    # remove duplicates
-    mtlf_df = remove_duplicates(mtlf_df)
-    mtrf_df = remove_duplicates(mtrf_df)
 
-    mtrf_df = mtrf_df[['GMTIntervalEnd', 'Wind_Forecast_MW', 'Solar_Forecast_MW']].set_index('GMTIntervalEnd').asfreq('H').sort_index()
-    mtrf_df = mtrf_df.join(mtlf_df[['GMTIntervalEnd','MTLF']].set_index('GMTIntervalEnd').asfreq('H').sort_index(), on='GMTIntervalEnd', how='outer').sort_values('GMTIntervalEnd').reset_index(drop=True)
-    mtrf_df['Ratio'] = (mtrf_df['Wind_Forecast_MW'] + mtrf_df['Solar_Forecast_MW']) / mtrf_df['MTLF']
-    mtrf_df.drop('MTLF', axis=1, inplace=True) 
+def prep_mtlf():
+    con = ibis.duckdb.connect("data/spp.ddb")
+    mtlf = con.table('mtlf')
+    drop_cols = ['Interval', 'GMTIntervalEnd',]
 
-    return fill_missed_values(mtrf_df)
-
-def add_enrgy_ratio_diff_to_mtrf(mtrf_df):
-    """
-    """
-    from darts.dataprocessing.transformers import Diff
-    # remove duplicates
-    mtrf_df = remove_duplicates(mtrf_df)
-
-    mtrf_df['Ratio_diff'] = mtrf_df['Ratio'] - mtrf_df['Ratio'].shift(1)
-    return fill_missed_values(mtrf_df)
-
-def create_mtrf_series(mtrf_ratio_df):
-    """
-    """
-    mtrf_series= TimeSeries.from_dataframe(mtrf_ratio_df, time_col='GMTIntervalEnd', value_cols=['Wind_Forecast_MW','Solar_Forecast_MW', 'Ratio'], fill_missing_dates=True, freq='H', fillna_value=0, static_covariates=None, hierarchy=None).astype(np.float32)
+    mtlf = (
+        mtlf
+        # .mutate(ds = _.timestamp_mst)
+        .drop(drop_cols) 
+        .order_by(['timestamp_mst'])
+    )
     
-    return mtrf_series      
+    return mtlf
 
 
-########################################
-# Preprocess series
-########################################
+def prep_all_df():
+    lmp = prep_lmp()
+    mtlf = prep_mtlf()
+    mtrf = prep_mtrf()
 
-def scale_series(series_train, series_val, series_all, global_fit=False):
-    """
-    """
-    # use global fit to do a single scaling
-    # this will allow us to do global forecasting
-    # for lmps
-    if global_fit:
-        scaler = series_train.pd_dataframe().abs().max().mean()
-    else:
-        scaler = series_train.pd_dataframe().abs().max()
+    # join into single dataset
+    all_df = (
+        mtlf
+        .left_join(mtrf, 'timestamp_mst')
+        .select(~s.contains("_right")) # remove 'dt_right'
+        .left_join(lmp, 'timestamp_mst')
+        .select(~s.contains("_right")) # remove 'dt_right'
+        .order_by(['unique_id', 'timestamp_mst'])
+    )
 
-    series_train_transformed = TimeSeries.from_dataframe(series_train.pd_dataframe()/scaler)
-    series_val_transformed = TimeSeries.from_dataframe(series_val.pd_dataframe()/scaler)
-    series_transformed = TimeSeries.from_dataframe(series_all.pd_dataframe()/scaler)
+    # engineer features
+    all_df = (
+        all_df
+        .drop_null(['unique_id'])
+        .mutate(re_ratio = (_.Wind_Forecast_MW + _.Solar_Forecast_MW) / _.MTLF)
+        .mutate(re_diff = _.re_ratio - _.re_ratio.lag(1))
+    )
 
-    return [series_train_transformed, series_val_transformed, series_transformed, scaler]
-
-def lmp_series_drop_horizon(lmp_series, start_time, forecast_horizon):
-    """
-    """
-    start_time_lmp = start_time
-    end_time_lmp = lmp_series.end_time() - pd.Timedelta(f'{forecast_horizon+1}H')
-    lmp_series = lmp_series.drop_before(start_time_lmp)
-    # lmp_series = lmp_series.drop_after(end_time_lmp)
-    return lmp_series
-
-def lmp_series_start_time(lmp_series, start_time):
-    """
-    """
-    lmp_series = lmp_series.drop_before(start_time)
-    return lmp_series
-
-def get_train_cutoff(lmp_series, forecast_horizon):
-    """
-    """
-    #keep last 30 days (720 data points + forecast_horizon) of target series for validation. 
-    training_cutoff = lmp_series[:-(720 + forecast_horizon)].end_time()
-    return training_cutoff
-
-def get_lmp_train_test_series(lmp_series_drop_horizon, training_cutoff, forecast_horizon, input_chunk_length):
-    """
-    """
-    lmp_series_train = lmp_series_drop_horizon.drop_after(training_cutoff)
-    lmp_series_val = lmp_series_drop_horizon.drop_before(training_cutoff)
-    lmp_series_all = lmp_series_drop_horizon
-
-    return [lmp_series_train, lmp_series_val, lmp_series_all]
+    return all_df
 
 
-############## mtlf #####################
-def get_mtlf_train_test_series(mtlf_series, start_time, training_cutoff, forecast_horizon, input_chunk_length):
-    """
-    """
-    # drop times before the starting time
-    mtlf_series = mtlf_series.drop_before(start_time)
-
-    # Split
-    mtlf_series_train = mtlf_series.drop_after(training_cutoff + pd.Timedelta(f'{forecast_horizon+1}H')) # for future covariates
-    mtlf_series_val = mtlf_series.drop_before(training_cutoff - pd.Timedelta(f'{input_chunk_length+1}H'))
-
-    return [mtlf_series_train, mtlf_series_val, mtlf_series]
-
-
-# def scale_mtlf_series(mtlf_series_train, mtlf_series_val, mtlf_series):
-#     """
-#     """
-#     transformer_mtlf = Scaler()
-#     mtlf_series_train_transformed = transformer_mtlf.fit_transform(mtlf_series_train)
-#     mtlf_series_val_transformed = transformer_mtlf.transform(mtlf_series_val)
-#     mtlf_series_transformed = transformer_mtlf.transform(mtlf_series)
-
-#     return [mtlf_series_train_transformed, mtlf_series_val_transformed, mtlf_series_transformed]
-
-############# avg_actual #################
-def get_avg_act_train_test_series(avg_act_series, start_time, training_cutoff):
-    """
-    """
-    avg_act_series = avg_act_series.drop_before(start_time)
-
-    # Split
-    avg_act_series_train = avg_act_series.drop_after(training_cutoff)
-    avg_act_series_val = avg_act_series.drop_before(training_cutoff)
-
-    return [avg_act_series_train, avg_act_series_val, avg_act_series]
-
-
-############## mtrf #####################
-def get_mtrf_train_test_series(mtrf_series, start_time, training_cutoff, forecast_horizon, input_chunk_length):
-    """
-    """
-    mtrf_series = mtrf_series.drop_before(start_time)
-
-    # Split
-    mtrf_series_train = mtrf_series.drop_after(training_cutoff + pd.Timedelta(f'{forecast_horizon+1}H')) # for future covariates
-    mtrf_series_val = mtrf_series.drop_before(training_cutoff - pd.Timedelta(f'{input_chunk_length+1}H'))
-
-    return [mtrf_series_train, mtrf_series_val, mtrf_series]
+def all_df_to_pandas(all_df):
+    all_df_pd = all_df.to_pandas()
+    all_df_pd.set_index('timestamp_mst', inplace=True)
+    all_df_pd = all_df_pd[IDS + Y + PAST_COLS + FUTR_COLS]
+    return all_df_pd
 
 
 
+def get_train_test_all(all_df_pd):
+    train_start = all_df_pd.index.min() + pd.Timedelta(f'{2*params.INPUT_CHUNK_LENGTH}h')
+    test_end = all_df_pd.index.max() - pd.Timedelta(f'{2*params.FORECAST_HORIZON}h')
+    tr_tst_split =  test_end - pd.Timedelta(f'{2*params.INPUT_CHUNK_LENGTH}h')
+    log.info(f'train_start: {train_start}')
+    log.info(f'tr_tst_split: {tr_tst_split}')
+    log.info(f'test_end: {test_end}')
+
+    train_idx = (all_df_pd.index < tr_tst_split) & (all_df_pd.index > train_start)
+    test_idx = (all_df_pd.index > tr_tst_split) & (all_df_pd.index < test_end)
+    
+    train_all = all_df_pd[train_idx]
+    test_all = all_df_pd[test_idx]
+
+    return train_all, test_all
+
+
+
+def fill_missing(series):
+    for i in range(len(series)):
+        transformer = MissingValuesFiller()
+        series[i] = transformer.transform(series[i])
+
+
+def get_all_series(all_df_pd):
+    all_series = TimeSeries.from_group_dataframe(
+        all_df_pd,
+        group_cols=IDS,
+        value_cols=Y,
+        fill_missing_dates=True,
+        freq='h',
+    )
+    
+    fill_missing(all_series) 
+    return all_series
+
+
+def get_train_series(train_all):
+    train_series = TimeSeries.from_group_dataframe(
+        train_all,
+        group_cols=IDS,
+        value_cols=Y,
+        fill_missing_dates=True,
+        freq='h',
+    )
+    fill_missing(train_series)
+    return train_series
+
+
+def get_test_series(test_all):
+    test_series = TimeSeries.from_group_dataframe(
+        test_all,
+        group_cols=IDS,
+        value_cols=Y,
+        fill_missing_dates=True,
+        freq='h',
+    )
+    fill_missing(test_series)
+    return test_series
+
+
+def get_futr_cov(all_df_pd):
+    futr_cov = TimeSeries.from_group_dataframe(
+        all_df_pd,
+        group_cols=IDS,
+        value_cols=FUTR_COLS,
+        fill_missing_dates=True,
+        freq='h',
+    )
+    fill_missing(futr_cov)
+    return futr_cov
+
+
+def get_past_cov(all_df_pd):
+    past_cov = TimeSeries.from_group_dataframe(
+        all_df_pd,
+        group_cols=IDS,
+        value_cols=PAST_COLS,
+        fill_missing_dates=True,
+        freq='h',
+    )
+    fill_missing(past_cov)
+    return past_cov
---
+++ src/modeling.py
@@ -0,0 +1,303 @@
+import os
+import shutil
+import pickle
+import random
+import sys
+import numpy as np
+import pandas as pd
+import duckdb
+from typing import List
+
+import requests
+from io import StringIO
+
+import ibis
+import ibis.selectors as s
+from ibis import _
+ibis.options.interactive = True
+
+from sklearn.preprocessing import RobustScaler
+
+from darts import TimeSeries, concatenate
+from darts.dataprocessing.transformers import (
+    Scaler,
+    MissingValuesFiller,
+    Mapper,
+    InvertibleMapper,
+)
+from darts.dataprocessing import Pipeline
+from darts.metrics import mape, smape, mae, ope, rmse
+from darts.utils.statistics import check_seasonality, plot_acf
+from darts.datasets import AirPassengersDataset, IceCreamHeaterDataset
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+from darts.utils.likelihood_models import QuantileRegression, GumbelLikelihood, GaussianLikelihood
+
+from darts import TimeSeries
+from darts.utils.timeseries_generation import (
+    gaussian_timeseries,
+    linear_timeseries,
+    sine_timeseries,
+)
+from darts.models import (
+    TFTModel,
+    TiDEModel,
+    DLinearModel,
+    NLinearModel,
+    TSMixerModel
+)
+
+
+from torchmetrics import (
+    SymmetricMeanAbsolutePercentageError, 
+    MeanAbsoluteError, 
+    MeanSquaredError,
+)
+
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# logging
+import logging
+
+# define log
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+from src import params
+
+
+import pprint
+# set up pretty printer
+pp = pprint.PrettyPrinter(indent=2, sort_dicts=False)
+
+
+def log_pretty(obj):
+    pretty_out = f"{pp.pformat(obj)}"
+
+    return f'{pretty_out}\n'
+    
+
+def build_fit_tsmixerx(
+    series: List[TimeSeries],
+    val_series: List[TimeSeries],
+    future_covariates: List[TimeSeries],
+    past_covariates: List[TimeSeries],
+    hidden_size: int=16,
+    ff_size: int=16,
+    num_blocks: int=16,
+    forecast_horizon: int=params.FORECAST_HORIZON,
+    input_chunk_length: int=params.INPUT_CHUNK_LENGTH,
+    lr: float=1.3e-4,
+    batch_size: int=64,
+    n_epochs: int=8,
+    dropout: float=0.41,
+    force_reset: bool=True, # reset model if already exists
+    callbacks=None,
+):
+    work_dir = os.getcwd() + '/model_checkpoints'
+    MODEL_TYPE = "ts_mixer_model"
+    quantiles = [0.01]+np.arange(0.05, 1, 0.05).tolist()+[0.99]
+    
+    #TODO: pick a metric...
+    torch_metrics = MeanAbsoluteError()
+    # torch_metrics = MeanSquaredError(squared=False)
+    # torch_metrics = SymmetricMeanAbsolutePercentageError() # don't use...
+    
+    encoders = {
+        "datetime_attribute": {
+            "future": ["hour", "dayofweek", "month"], # 
+            "past": ["hour", "dayofweek", "month"], # 
+        },
+        "position": {
+            "past": ["relative"], 
+            "future": ["relative"]
+        },
+        "transformer": Scaler(RobustScaler(), global_fit=True)
+    }
+
+    # common parameters across models
+    model_params = {
+        'hidden_size': hidden_size,
+        'ff_size': ff_size,
+        'num_blocks': num_blocks,
+        'input_chunk_length': input_chunk_length,
+        'output_chunk_length': forecast_horizon,
+        'batch_size': batch_size,
+        'n_epochs': n_epochs,
+        'dropout': dropout,
+        'add_encoders': encoders,
+        'likelihood': QuantileRegression(quantiles=quantiles),  # QuantileRegression is set per default
+        'optimizer_kwargs': {"lr": lr},
+        'random_state': 42,
+        'torch_metrics': torch_metrics,
+        'use_static_covariates': False,
+        'save_checkpoints': True,
+        'work_dir': work_dir,
+        'model_name': MODEL_TYPE, # used for checkpoint saves
+        'force_reset': force_reset, # reset model if already exists
+        'log_tensorboard': True,
+    }
+
+    # throughout training we'll monitor the validation loss for early stopping
+    early_stopper = EarlyStopping("val_loss", min_delta=0.01, patience=3, verbose=True)
+    if callbacks is None:
+        callbacks = [early_stopper]
+    else:
+        callbacks = [early_stopper] + callbacks
+
+    pl_trainer_kwargs = {"callbacks": callbacks}
+    # model_params['pl_trainer_kwargs'] = pl_trainer_kwargs
+    log.info(f'model_params: \n{log_pretty(model_params)}')
+    
+    model = TSMixerModel(**model_params)
+
+    # train the model
+    fit_params = {
+        'series': series,
+        'val_series': val_series,
+        'future_covariates': future_covariates,
+        'past_covariates': past_covariates,
+        'val_future_covariates': future_covariates,
+        'val_past_covariates': past_covariates,
+    }
+    model.fit(**fit_params)
+
+    # reload best model over course of training
+    model = TSMixerModel.load_from_checkpoint(
+        work_dir=work_dir,
+        model_name=MODEL_TYPE
+    )
+    
+    model.MODEL_TYPE = MODEL_TYPE
+    model.TRAIN_TIMESTAMP = pd.Timestamp.utcnow()
+
+    return model
+
+
+
+def build_fit_tft(
+    series: List[TimeSeries],
+    val_series: List[TimeSeries],
+    future_covariates: List[TimeSeries],
+    past_covariates: List[TimeSeries],
+    hidden_size: int=12, # Hidden state size of the TFT. It is the main hyper-parameter and common across the internal TFT architecture.
+    lstm_layers: int = 1, # Number of layers for the Long Short Term Memory (LSTM) Encoder and Decoder (1 is a good default).
+    num_attention_heads: int=2, # Number of attention heads (4 is a good default)
+    dropout: float=0.1,
+    forecast_horizon: int=params.FORECAST_HORIZON,
+    input_chunk_length: int=params.INPUT_CHUNK_LENGTH,
+    lr: float=2.5e-4,
+    batch_size: int=64,
+    n_epochs: int=6,
+    force_reset: bool=True, # reset model if already exists
+    callbacks=None,
+):
+    work_dir = os.getcwd() + '/model_checkpoints'
+    MODEL_TYPE = "tft_model"
+    quantiles = [0.01]+np.arange(0.05, 1, 0.05).tolist()+[0.99]
+    
+    #TODO: pick a metric...
+    torch_metrics = MeanAbsoluteError()
+    # torch_metrics = MeanSquaredError(squared=False)
+    # torch_metrics = SymmetricMeanAbsolutePercentageError() # don't use...
+    
+    encoders = {
+        "datetime_attribute": {
+            "future": ["hour", "dayofweek", "month"], # 
+            "past": ["hour", "dayofweek", "month"], # 
+        },
+        "position": {
+            "past": ["relative"], 
+            "future": ["relative"]
+        },
+        "transformer": Scaler(RobustScaler(), global_fit=True)
+    }
+
+    # common parameters across models
+    model_params = {
+        'hidden_size': hidden_size,
+        'lstm_layers': lstm_layers,
+        'num_attention_heads': num_attention_heads,
+        'dropout': dropout,
+        'input_chunk_length': input_chunk_length,
+        'output_chunk_length': forecast_horizon,
+        'batch_size': batch_size,
+        'n_epochs': n_epochs,
+        'add_encoders': encoders,
+        'likelihood': QuantileRegression(quantiles=quantiles),  # QuantileRegression is set per default
+        'optimizer_kwargs': {"lr": lr},
+        'random_state': 42,
+        'torch_metrics': torch_metrics,
+        'use_static_covariates': False,
+        'save_checkpoints': True,
+        'work_dir': work_dir,
+        'model_name': MODEL_TYPE, # used for checkpoint saves
+        'force_reset': force_reset, # reset model if already exists
+        'log_tensorboard': True,
+    }
+    
+
+    # throughout training we'll monitor the validation loss for early stopping
+    early_stopper = EarlyStopping("val_loss", min_delta=0.01, patience=3, verbose=True)
+    if callbacks is None:
+        callbacks = [early_stopper]
+    else:
+        callbacks = [early_stopper] + callbacks
+
+    pl_trainer_kwargs = {"callbacks": callbacks}
+    # model_params['pl_trainer_kwargs'] = pl_trainer_kwargs
+    log.info(f'model_params: \n{log_pretty(model_params)}')
+
+    model = TFTModel(**model_params)
+
+    # train the model
+    fit_params = {
+        'series': series,
+        'val_series': val_series,
+        'future_covariates': future_covariates,
+        'past_covariates': past_covariates,
+        'val_future_covariates': future_covariates,
+        'val_past_covariates': past_covariates,
+    }
+    model.fit(**fit_params)
+
+    # reload best model over course of training
+    model = TFTModel.load_from_checkpoint(
+        work_dir=work_dir,
+        model_name=MODEL_TYPE
+    )
+    
+    model.MODEL_TYPE = MODEL_TYPE
+    model.TRAIN_TIMESTAMP = pd.Timestamp.utcnow()
+
+    return model
+
+
+
+
+def get_ci_err(actual_series, pred_series, n_jobs=1, verbose=False):
+    ci_cover_err = []
+    for i, pred in enumerate(pred_series):
+        
+        series_qs = pred.quantiles_df((0.1, 0.9))
+        val_y = actual_series[i].pd_dataframe()
+        
+        eval_df = series_qs.merge(
+            val_y,
+            how='inner',
+            left_index=True,
+            right_index=True,
+        )
+    
+    
+        cover = (
+            (eval_df['LMP_0.9'] > eval_df['LMP']) &
+            (eval_df['LMP_0.1'] < eval_df['LMP'])
+        ).mean() # should be about 80%
+
+        ci_cover_err += [100 * np.abs(cover - 0.8)]
+
+    return ci_cover_err
