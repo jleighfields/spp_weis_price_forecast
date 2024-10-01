948b2b0815fbe7f3bde4650b6a6d142b38d50abd
Justin Fields <jleighfields@gmail.com>
2024-10-01 22:36:21 +0000

    job test

--- jobs/data_collection_hourly_job.py
+++ jobs/data_collection_hourly_job.py
@@ -12,23 +12,28 @@ load_dotenv()  # take environment variables from .env.
 # Start the studio
 s = Studio(name="data-collection", teamspace="spp-weis", user="jleighfields-yst2q")
 print("starting Studio...")
-s.start()
+s.start(machine=Machine.CPU)
 
 # Install plugin if not installed (in this case, it is already installed)
-s.install_plugin("jobs")
+# s.install_plugin("jobs")
 
-jobs_plugin = s.installed_plugins["jobs"]
+# jobs_plugin = s.installed_plugins["jobs"]
 
 # --------------------------
 # Example 1: Submit a job
 # --------------------------
 
 command = '''
-cd ~/spp_weis_price_forecast
-python scripts/data_collection/data_collection_hourly.py
-'''
+# cd ~/spp_weis_price_forecast
+# python scripts/data_collection/data_collection_hourly.py
+# '''
 
-jobs_plugin.run(command, name=f"hourly_{now_utc}", machine=Machine.CPU)
+# jobs_plugin.run(command, name=f"hourly_{now_utc}", machine=Machine.CPU)
+
+
+# jobs_plugin.run(command, name=f"lmp_daily_{now_utc}", machine=Machine.CPU)
+
+s.run("cd ~/spp_weis_price_forecast && python scripts/data_collection/data_collection_hourly.py")
 
 
 print("Stopping Studio")
