ad309b048f19aec184aa2ea595df1b430827df25
Justin Fields <jleighfields@gmail.com>
2024-10-03 12:38:59 +0000

    update daily script

--- jobs/data_collection_lmp_daily_job.py
+++ jobs/data_collection_lmp_daily_job.py
@@ -8,6 +8,6 @@ load_dotenv()  # take environment variables from .env.
 print("starting Studio...")
 s = Studio(name="model-train", teamspace="spp-weis", user="jleighfields-yst2q")
 s.start(machine=Machine.CPU)
-s.run("cd ~/spp_weis_price_forecast && python scripts/model_retrain.py")
+s.run("cd ~/spp_weis_price_forecast && python scripts/data_collection/data_collection_daily.py")
 print("Stopping Studio")
 s.stop()
