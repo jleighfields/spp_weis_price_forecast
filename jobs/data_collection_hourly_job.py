5fe60144f5f1ee8e21c8ee74fbfdcc513195da1b
Justin Fields <jleighfields@gmail.com>
2024-10-12 12:11:53 +0000

    added error handling to jobs

--- jobs/data_collection_hourly_job.py
+++ jobs/data_collection_hourly_job.py
@@ -3,11 +3,23 @@
 
 from lightning_sdk import Machine, Studio, JobsPlugin, MultiMachineTrainingPlugin
 from dotenv import load_dotenv
+import time
 load_dotenv()  # take environment variables from .env.
 
 print("starting Studio...")
 s = Studio(name="data-collection", teamspace="spp-weis", user="jleighfields-yst2q")
 s.start(machine=Machine.CPU)
-s.run("cd ~/spp_weis_price_forecast && python scripts/data_collection/data_collection_hourly.py")
+for i in range(30):
+    print(f'status: {s.status} - count: {i}')
+    if s.status == 'Running':
+        break
+
+    time.sleep(30)
+
+try:
+    s.run("cd ~/spp_weis_price_forecast && python scripts/data_collection/data_collection_hourly.py")
+except Exception as e:
+        print("command failed with error: ", e)
+
 print("Stopping Studio")
 s.stop()
--- jobs/model_retrain_job.py
+++ jobs/model_retrain_job.py
@@ -3,11 +3,24 @@
 
 from lightning_sdk import Machine, Studio, JobsPlugin, MultiMachineTrainingPlugin
 from dotenv import load_dotenv
+import time
 load_dotenv()  # take environment variables from .env.
 
 print("starting Studio...")
 s = Studio(name="data-collection", teamspace="spp-weis", user="jleighfields-yst2q")
 s.start(machine=Machine.T4)
-s.run("cd ~/spp_weis_price_forecast && python scripts/model_retrain.py")
+for i in range(30):
+    print(f'status: {s.status} - count: {i}')
+    if s.status == 'Running':
+        break
+
+    time.sleep(30)
+
+
+try:
+    s.run("cd ~/spp_weis_price_forecast && python scripts/model_retrain.py")
+except Exception as e:
+        print("command failed with error: ", e)
+
 print("Stopping Studio")
 s.stop()
