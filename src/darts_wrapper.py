262db3e056be4f4ca65d3c2b51c914f783948a9e
Justin Fields <jleighfields@gmail.com>
2024-10-29 06:52:43 -0600

    format darts_wrapper

--- src/darts_wrapper.py
+++ src/darts_wrapper.py
@@ -1,20 +1,12 @@
-# see for custom model set up
-# https://github.com/mlflow/mlflow/blob/74d75109aaf2975f5026104d6125bb30f4e3f744/mlflow/pytorch.py
+'''
+Module for custom Darts model serving using mlflow pyfunc
+'''
 
-# for custom sktime model
-# https://mlflow.org/docs/latest/models.html#example-creating-a-custom-sktime-flavor
-# https://changhsinlee.com/mlflow-custom-flavor/
-
-import os
 import logging
 import numpy as np
-import pandas as pd
-import mlflow
 import mlflow.pyfunc
-import time
 import torch
 from darts import TimeSeries
-
 
 # define log
 logging.basicConfig(level=logging.INFO)
@@ -27,25 +19,16 @@ class DartsGlobalModel(mlflow.pyfunc.PythonModel):
         from darts.models import (
             TFTModel,
             TiDEModel,
-            DLinearModel,
-            NLinearModel,
             TSMixerModel,
             NaiveEnsembleModel,
-            RegressionEnsembleModel,
         )
         import pickle
         # print(f'context.artifacts: {context.artifacts}')
-        # device = 0 if torch.cuda.is_available() else -1
 
         # load model type
         with open(context.artifacts["MODEL_TYPE.pkl"], 'rb') as handle:
             self.MODEL_TYPE = pickle.load(handle)
         log.info(f'MODEL_TYPE: {self.MODEL_TYPE}')
-
-        
-        # load scalers
-        # with open(context.artifacts["scalers"], 'rb') as handle:
-        #     self.scalers = pickle.load(handle)
 
         # load model based on MODEL_TYPE
         if self.MODEL_TYPE == "tft_model":
@@ -53,23 +36,12 @@ class DartsGlobalModel(mlflow.pyfunc.PythonModel):
             
         elif self.MODEL_TYPE == "tide_model":
             self.model = TiDEModel.load(context.artifacts["model"], map_location=torch.device('cpu'))
-            
-        elif self.MODEL_TYPE == "dlinear_model":
-            self.model = DLinearModel.load(context.artifacts["model"], map_location=torch.device('cpu'))
-            
-        elif self.MODEL_TYPE == "nlinear_model":
-            self.model = NLinearModel.load(context.artifacts["model"], map_location=torch.device('cpu'))
 
         elif self.MODEL_TYPE == "ts_mixer_model":
             self.model = TSMixerModel.load(context.artifacts["model"], map_location=torch.device('cpu'))
 
         elif self.MODEL_TYPE == 'naive_ens':
             self.model = NaiveEnsembleModel.load(context.artifacts["model"])
-
-        elif self.MODEL_TYPE == 'reg_ens':
-            self.model = RegressionEnsembleModel.load(context.artifacts["model"])
-
-        # TODO: add ensemble models
             
         else:
             raise ValueError(f'Unsuported MODEL_TYPE: {self.MODEL_TYPE}')
@@ -105,20 +77,6 @@ class DartsGlobalModel(mlflow.pyfunc.PythonModel):
         forecast_horizon = model_input['n'].item()
         num_samples = model_input['num_samples'].item()
 
-        
-        # scale time series
-        # log.info('SCALING INPUTS...')
-        # series_scaled = TimeSeries.from_dataframe(
-        #     series.pd_dataframe()/self.scalers['series']
-        #     )
-
-        # past_covariates_scaled = TimeSeries.from_dataframe(
-        #     past_covariates.pd_dataframe()/self.scalers['pc']
-        #     )
-        
-        # future_covariates_scaled = TimeSeries.from_dataframe(
-        #     future_covariates.pd_dataframe()/self.scalers['fc']
-        #     )
 
         log.info('RUNNING PREDICT...')
         pred_series = self.model.predict(
@@ -134,8 +92,4 @@ class DartsGlobalModel(mlflow.pyfunc.PythonModel):
             )
 
         return TimeSeries.to_json(pred_series)
-    
-# Changed the output format of prediction function due to the folowing exception error:
-# Exception: Request failed with status 400, 
-# {"error_code": "BAD_REQUEST", "message": "Encountered an unexpected error while converting model response to JSON.Error 
-# 'Object of type TimeSeries is not JSON serializable'"
+
