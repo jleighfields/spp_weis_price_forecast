d78503a95f6761b114d5d1f1bfb21ec6d9d3c1cd
Justin Fields <jleighfields@gmail.com>
2024-11-28 15:09:29 +0000

    reset to tsmixer

--- scripts/model_retrain.py
+++ scripts/model_retrain.py
@@ -119,16 +119,16 @@ darts_signature = infer_signature(df, ouput_example)
 
 # build pretrained models
 models_tsmixer = []
-# for i, param in enumerate(parameters.TSMIXER_PARAMS):
-#     print(f'\ni: {i} \t' + '*' * 25, flush=True)
-#     model_tsmixer = build_fit_tsmixerx(
-#         series=train_test_all_series,
-#         val_series=test_series,
-#         future_covariates=futr_cov,
-#         past_covariates=past_cov,
-#         **param
-#     )
-#     models_tsmixer += [model_tsmixer]
+for i, param in enumerate(parameters.TSMIXER_PARAMS):
+    print(f'\ni: {i} \t' + '*' * 25, flush=True)
+    model_tsmixer = build_fit_tsmixerx(
+        series=train_test_all_series,
+        val_series=test_series,
+        future_covariates=futr_cov,
+        past_covariates=past_cov,
+        **param
+    )
+    models_tsmixer += [model_tsmixer]
 
 models_tide = []
 # for i, param in enumerate(parameters.TIDE_PARAMS):
@@ -143,16 +143,16 @@ models_tide = []
 #     models_tide += [model_tide]
 
 models_tft = []
-for i, param in enumerate(parameters.TFT_PARAMS):
-    print(f'\ni: {i} \t' + '*' * 25, flush=True)
-    model_tft = build_fit_tft(
-        series=train_test_all_series,
-        val_series=test_series,
-        future_covariates=futr_cov,
-        past_covariates=past_cov,
-        **param
-    )
-    models_tft += [model_tft]
+# for i, param in enumerate(parameters.TFT_PARAMS):
+#     print(f'\ni: {i} \t' + '*' * 25, flush=True)
+#     model_tft = build_fit_tft(
+#         series=train_test_all_series,
+#         val_series=test_series,
+#         future_covariates=futr_cov,
+#         past_covariates=past_cov,
+#         **param
+#     )
+#     models_tft += [model_tft]
 
 # Refit and log model with best params
 log.info('log ensemble model')
