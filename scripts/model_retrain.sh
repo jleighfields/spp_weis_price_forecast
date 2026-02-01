24190f56c4d84aa81c6cc8af81c83535df7442ff
Justin Fields <jleighfields@gmail.com>
2026-01-31 17:55:59 -0700

    Add email notification on retrain failure and set 1hr timeout

--- scripts/model_retrain.sh
+++ scripts/model_retrain.sh
@@ -1,4 +1,13 @@
-cd ~/Documents/github/spp_weis_price_forecast
-mkdir -p ./scripts/model_retrain/
+#!/bin/bash
+
+NOTIFY_EMAIL="jleighfields@gmail.com"
+PROJECT_DIR="$HOME/Documents/github/spp_weis_price_forecast"
+
+cd "$PROJECT_DIR"
+mkdir -p ./scripts/model_retrains/
 NOW=$(date -u +"%Y-%m-%dT%H-%M-%S")
-uv run papermill ./scripts/model_retrain.ipynb "./scripts/model_retrains/${NOW}_model_retrain.ipynb"
+OUTPUT_NOTEBOOK="./scripts/model_retrains/${NOW}_model_retrain.ipynb"
+
+if ! uv run papermill ./scripts/model_retrain.ipynb "$OUTPUT_NOTEBOOK" --execution-timeout 3600 2>&1; then
+    echo "Model retrain failed at $NOW" | mail -s "Model retrain job FAILED - $NOW" "$NOTIFY_EMAIL"
+fi
